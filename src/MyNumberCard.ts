401cdb4607cef548104cb9cf201f60094d77cabe
Aruneko <aruneko99@gmail.com>
2019-03-16 14:15:15 +0900

    マイナンバー読み取り機能の実装

--- src/MyNumberCard.ts
+++ src/MyNumberCard.ts
@@ -45,6 +45,11 @@ export class MyNumberCard {
     return this.device.sendCommand(command)
   }
 
+  private async selectCardInfoAP(): Promise<Type4BPacket> {
+    const cardInfoAP = Uint8Array.of(0xD3, 0x92, 0x10, 0x00, 0x31, 0x00, 0x01, 0x01, 0x04, 0x08)
+    return this.selectDF(cardInfoAP)
+  }
+
   private async selectCertAP(): Promise<Type4BPacket> {
     const certAP = Uint8Array.of(
       0xd3,
@@ -61,7 +66,15 @@ export class MyNumberCard {
     return this.selectDF(certAP)
   }
 
-  private async selectRSAPrivateKeyEF(): Promise<Type4BPacket> {
+  private async selectMyNumberEF(): Promise<Type4BPacket> {
+    return this.selectEF(Uint8Array.of(0x00, 0x01))
+  }
+
+  private async selectCardInfoPinEF(): Promise<Type4BPacket> {
+    return this.selectEF(Uint8Array.of(0x00, 0x11))
+  }
+
+  private async selectRSAPrivateKeyPinEF(): Promise<Type4BPacket> {
     return this.selectEF(Uint8Array.of(0x00, 0x18))
   }
 
@@ -87,6 +100,61 @@ export class MyNumberCard {
     return parser.size
   }
 
+  private async readBinary(size: number): Promise<Uint8Array> {
+    const result = new Uint8Array(size)
+    let position = 0
+
+    while (position < size) {
+      // 256で割ったあまりをlengthとする
+      let length = 0
+      if (size - position > 0xff) {
+        length = 0
+      } else {
+        length = size - position
+      }
+      // バイナリをを読み込むコマンドを発行
+      const readBinaryCommand = Uint8Array.of(
+        0x00,
+        0xb0,
+        (position >> 8) & 0xff,
+        position & 0xff,
+        length
+      )
+      const response = await this.device.sendCommand(
+        readBinaryCommand
+      )
+      // 読み込んだバイナリを後ろにつなげていく
+      result.set(response.data, position)
+      position += response.data.byteLength
+    }
+    return result
+  }
+
+  public async getMyNumber(pin: string): Promise<string> {
+    // マイナンバーカードに接続
+    await this.device.connectToCard()
+
+    // Select DF
+    await this.selectCardInfoAP()
+
+    // Select Pin EF
+    await this.selectCardInfoPinEF()
+
+    // Verify Pin
+    await this.verifyPin(pin)
+
+    // Select My Number EF
+    await this.selectMyNumberEF()
+
+    // Read my number
+    const myNumber = await this.readBinary(16)
+
+    // 通信終了
+    await this.disconnect()
+
+    return String.fromCharCode(...myNumber.slice(3, 15))
+  }
+
   public async signMessageWithPrivateKey(hashType: HashType, pin: string, message: string): Promise<Uint8Array> {
     // マイナンバーカードに接続
     await this.device.connectToCard()
@@ -94,8 +162,8 @@ export class MyNumberCard {
     // Select DF
     await this.selectCertAP()
 
-    // Select EF
-    await this.selectRSAPrivateKeyEF()
+    // Select Pin EF
+    await this.selectRSAPrivateKeyPinEF()
 
     // Verify PIN
     await this.verifyPin(pin)
@@ -112,38 +180,6 @@ export class MyNumberCard {
     return signedMessage.data
   }
 
-  private async getPublicKeyFromCard(publicKeyLength: number): Promise<Uint8Array> {
-    console.info('===== get public key =====')
-    // 公開鍵を格納する変数を定義
-    const publicKey = new Uint8Array(publicKeyLength)
-    let position = 0
-
-    while (position < publicKeyLength) {
-      // 256で割ったあまりをlengthとする
-      let length = 0
-      if (publicKeyLength - position > 0xff) {
-        length = 0
-      } else {
-        length = publicKeyLength - position
-      }
-      // 公開鍵を読み込むコマンドを発行
-      const readPublicKeyCommand = Uint8Array.of(
-        0x00,
-        0xb0,
-        (position >> 8) & 0xff,
-        position & 0xff,
-        length
-      )
-      const response = await this.device.sendCommand(
-        readPublicKeyCommand
-      )
-      // 読み込んだ公開鍵を後ろにつなげていく
-      publicKey.set(response.data, position)
-      position += response.data.byteLength
-    }
-    return publicKey
-  }
-
   public async getPublicKey() {
     // マイナンバーカードに接続
     await this.device.connectToCard()
@@ -158,13 +194,7 @@ export class MyNumberCard {
     const publicKeyLength = await this.checkPublicKeyLength()
 
     // Get Public Key
-    const publicKey = await this.getPublicKeyFromCard(publicKeyLength)
-    console.log(
-      'publicKey: ',
-      publicKey.reduce((memo: string, i: number) => {
-        return memo + ', 0x' + ('0' + i.toString(16)).slice(-2)
-      }, '')
-    )
+    const publicKey = await this.readBinary(publicKeyLength)
 
     // 通信終了
     await this.disconnect()
